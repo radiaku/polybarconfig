25959f4b9020a6025b4f6baadfa3fe0c0e32f1b0
radiaku <aku.radiaku@gmail.com>
2025-12-01 10:53:45 +0700

    fixing launcher

--- launch.sh
+++ launch.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env sh
 
-# Terminate already running bar instances
+# Kill running Polybar
 killall -q polybar
 
-# Wait until the processes have been shut down
+# Wait for shutdown
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# for multimonitor
-if type "xrandr"; then
+# Force Polybar to use X11 inside Wayland
+export GDK_BACKEND=x11
+export QT_QPA_PLATFORM=xcb
+export SDL_VIDEODRIVER=x11
+export CLUTTER_BACKEND=x11
+
+# Critical: remove Wayland DISPLAY variable
+unset WAYLAND_DISPLAY
+
+# Optional: prevent Hyprland from intercepting clicks
+export HYPRLAND_NO_FRACTIONAL_SCALE=1
+
+# Launch bar on each monitor
+if type "xrandr" >/dev/null 2>&1; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example & disown
+    MONITOR=$m polybar --reload example &
   done
 else
-  polybar --reload example & disown
+  polybar --reload example &
 fi
